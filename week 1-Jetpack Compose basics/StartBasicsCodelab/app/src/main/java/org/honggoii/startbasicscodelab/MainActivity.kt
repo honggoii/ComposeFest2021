34d38ca3111bc1f96851c1dd0bda0c9c5a41387f
honggoii <honggoii@naver.com>
2021-11-13 16:21:43 +0900

    [Jetpack Compose basics] Reusing composables

--- week 1-Jetpack Compose basics/StartBasicsCodelab/app/src/main/java/org/honggoii/startbasicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/StartBasicsCodelab/app/src/main/java/org/honggoii/startbasicscodelab/MainActivity.kt
@@ -3,11 +3,14 @@ package org.honggoii.startbasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.honggoii.startbasicscodelab.ui.theme.StartBasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,18 +18,28 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             StartBasicsCodelabTheme {
+                MyApp()
                 // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+//                Surface(color = MaterialTheme.colors.background) {
+//                    Greeting("Android")
+//                }
             }
         }
     }
 }
 
 @Composable
+private fun MyApp() {
+    Surface(color = MaterialTheme.colors.background) {
+        Greeting("Android")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
